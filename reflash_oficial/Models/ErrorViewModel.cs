880e4c3362220e2a1a5b73ca6ef1a8964ce86fe4
ProstoMaximka14 <147098815+ProstoMaximka14@users.noreply.github.com>
2026-01-26 00:56:06 +0300

    Добавьте файлы проекта.

---
+++ reflash_oficial/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace reflash_oficial.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
